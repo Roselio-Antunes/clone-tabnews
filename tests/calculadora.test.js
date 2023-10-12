df5f012decb55bab63a85e8b8db95d2a9431f4eb
Roselio-Antunes <62839461+Roselio-Antunes@users.noreply.github.com>
2023-10-12 20:56:45 +0000

    calculadoras salvas

---
+++ tests/calculadora.test.js
@@ -0,0 +1,6 @@
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calaculadora.somar(2, 2);
+  console.log(resultado);
+});
